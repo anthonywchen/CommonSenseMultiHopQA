79c8da9cca3a2126be9f7dc70e38871e0354710c
Anthony Chen <anthony.w.chen@outlook.com>
2019-05-26 17:55:48 -0700

    added in code to compute bert score. Also needed to add in python3 requirements since bertscore requires python3

---
+++ src/pycocoevalcap/bert_score/bert_scorer.py
@@ -0,0 +1,61 @@
+import argparse
+from bert_score import score
+
+def compute_reference_scores(candidates_file, references_file):
+	""" Computes the BERTScore with respect to a single references file """
+
+	# Load in candidates 
+	cands = []
+	with open(candidates_file) as f:
+		for line in f:
+			cands.append(line.strip())
+
+	# Load in references
+	refs = []
+	with open(references_file) as f:
+		for line in f:
+			refs.append(line.strip()) 
+
+	# Check that number of candidates matches number of references
+	assert len(cands) == len(refs)
+
+	# Compute BERTScore
+	_, _, bert_scores = score(cands, refs, bert="bert-base-uncased", verbose=True)
+
+	return bert_scores
+
+
+def bert_scorer(candidates_file, references_file1, references_file2=None):
+	""" Computes the BERTScore with respect to each reference file 
+		and merges the scores from the two reference files by doing a argmax operation.
+		The scores are then written to file under the name ```<candidates_file>.bert_score```
+	"""
+
+	# Get BERTScore with respect to first reference file
+	cands_refs1_score = compute_reference_scores(candidates_file, references_file1)
+
+	# Get BERTScore with respect to second reference file if second reference file exists
+	if references_file2:
+		cands_refs2_score = compute_reference_scores(candidates_file, references_file2)
+		# Argmax over the scores from the two references file
+		assert len(cands_refs1_score) == len(cands_refs2_score)
+		cands_score = []
+		for s1, s2 in zip(cands_refs1_score, cands_refs2_score):
+		    cands_score.append(max(s1, s2))
+	else:
+		cands_score = cands_refs1_score
+
+	# Write the scores to file
+	with open(candidates_file + '-bert_score.txt', 'w') as f:
+		for c in cands_score:
+			f.write(str(c.item()) + '\n')
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--candidates_file', type=str, required=True, help='path to file with generated answers')
+	parser.add_argument('--references_file1', type=str, required=True, help='path to file of reference answers')
+	parser.add_argument('--references_file2', type=str, required=False, default=None, help='path to file of (second) reference answers')
+
+	args = parser.parse_args()
+
+	bert_scorer(args.candidates_file, args.references_file1, args.references_file2)
